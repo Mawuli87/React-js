a22ecf1d36b1eb80b29fd30ea34e64bff02ee35f
Mawuli87 <yawomessie@gmail.com>
2024-08-16 00:20:03 +0000

    Props and Prop Drilling

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { TaskCard } from "./TaskCard";
+import { BoxCard } from "./BoxCard";
 
-export const TaskList = () => {
+export const TaskList = ({ concept }) => {
   const [tasks, setTasks] = useState([
     { id: 5271, name: "Record React Lectures", completed: true },
     { id: 7825, name: "Edit React Lectures", completed: false },
@@ -21,19 +23,37 @@ export const TaskList = () => {
         </button>
         {show &&
           tasks.map((task) => (
-            <li
+            <TaskCard
               key={task.id}
-              className={task.completed ? "completed" : "incomplete"}
-            >
-              <span>
-                {task.id} - {task.name}
-              </span>
-              <button onClick={() => handleDelete(task.id)} className="delete">
-                Delete
-              </button>
-            </li>
+              concept={concept}
+              task={task}
+              handleDelete={handleDelete}
+            />
           ))}
       </ul>
+      <BoxCard result="success">
+        <p className="title">Lorem ipsum dolor sit amet.</p>
+        <p className="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+          vel!
+        </p>
+      </BoxCard>
+
+      <BoxCard result="warning">
+        <p className="title">Lorem ipsum dolor sit.</p>
+        <p className="description">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, eum
+          odit! Labore eaque fuga repellat adipisci molestias quasi animi fugiat
+          necessitatibus sunt vel, optio rem non quidem! Blanditiis cupiditate
+          iusto omnis reprehenderit assumenda maxime, nam perferendis impedit
+          libero odit eius eum aut cum ad, excepturi officiis repudiandae.
+          Molestiae, eum cumque?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+          nulla?
+        </p>
+      </BoxCard>
     </>
   );
 };
